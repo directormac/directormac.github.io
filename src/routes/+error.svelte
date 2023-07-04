57e442abc7607f0e22589864e1f8e1f02880e06a
directormac <markasena@gmail.com>
2023-07-05 02:51:19 +0800

    Added a workflow for github actions instead of manually calling it

---
+++ src/routes/+error.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<h1>{$page.url} : {$page.error?.message}</h1>
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -11,11 +12,17 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			trailing
+		}),
 		alias: {
 			$components: 'src/lib/components',
 			$stores: 'src/lib/stores',
 			$utils: 'src/lib/utils'
+		},
+		//Incase you changed the basepath please uncomment the following lines below
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH
 		}
 	}
 };
