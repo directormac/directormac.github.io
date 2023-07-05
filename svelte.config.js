82c32c281f3991c8967a45c155c6376de7b3ba5b
directormac <markasena@gmail.com>
2023-07-05 17:03:31 +0800

    fix path

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const BASE_PATH = 'directormac.github.io';
+const BASE_PATH = '';
 const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
